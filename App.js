27374c9d0a27d3663666019affb6b57beb69ad5d
isaiah francois <francoisisaiaht@yahoo.com>
2020-10-20 10:06:22 -0400

    Added redux to app

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Map from './pages/Map'
 
+// Redux
+import { createStore, applyMiddleware } from "redux";
+import { Provider as StoreProvider } from "react-redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
 // Navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -9,7 +15,11 @@ import { createStackNavigator } from "@react-navigation/stack";
 const Stack = createStackNavigator();
 
 export default function App() {
+  const middleware = [thunk];
+  const store = createStore(rootReducer, applyMiddleware(...middleware));
+
   return (
+    <StoreProvider store={store}>
     <NavigationContainer>
           <Stack.Navigator>
             <Stack.Screen
@@ -18,6 +28,7 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
+    </StoreProvider>
   );
 }
 
